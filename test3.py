84d9dfe1d9256888af47ffd25b31b7097c6f243b
Jörg Lang <joerg.l@outlook.de>
2024-07-03 19:31:27 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -6,4 +6,4 @@ KVUri = os.environ['KEYVAULT_URI']
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
 retrieved_secret = client.get_secret('example-secret')
-print("Secret Value:" + retrieved_secret)
+print("Secret Value: ", retrieved_secret)
