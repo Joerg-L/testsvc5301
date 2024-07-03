db94318753dde593aa511556ea8f18fb0d771b1c
Jörg Lang <joerg.l@outlook.de>
2024-07-03 19:11:38 +0200

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,8 @@
+import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+KVUri = os.environ['KEYVAULT_URI']
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+retrieved_secret = client.get_secret('example-secret')
